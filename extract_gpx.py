49999eebbb355e94e7a4c7d0d28a827ea405092f
Rob <robgroves0.gmailcom>
2021-07-31 20:43:18 -0400

    feat: Add thinDistance argument that cuts out points spaced less than thinDistance kilometers apart.

--- extract_gpx.py
+++ extract_gpx.py
@@ -20,6 +20,7 @@ It defines classes_and_methods
 import sys
 import os
 import re
+import math
 
 
 from argparse import ArgumentParser
@@ -45,6 +46,124 @@ class CLIError(Exception):
         return self.msg
     def __unicode__(self):
         return self.msg
+    
+class GeoLocation:
+    '''
+    Class representing a coordinate on a sphere, most likely Earth.
+    
+    This class is based from the code smaple in this paper:
+        http://janmatuschek.de/LatitudeLongitudeBoundingCoordinates
+        
+    The owner of that website, Jan Philip Matuschek, is the full owner of 
+    his intellectual property. This class is simply a Python port of his very
+    useful Java code. All code written by Jan Philip Matuschek and ported by Jeremy Fein (jfein) 
+    (which is all of this class) is owned by Jan Philip Matuschek.
+    '''
+ 
+ 
+    MIN_LAT = math.radians(-90)
+    MAX_LAT = math.radians(90)
+    MIN_LON = math.radians(-180)
+    MAX_LON = math.radians(180)
+    
+    EARTH_RADIUS = 6378.1  # kilometers
+    
+    
+    @classmethod
+    def from_degrees(cls, deg_lat, deg_lon):
+        rad_lat = math.radians(deg_lat)
+        rad_lon = math.radians(deg_lon)
+        return GeoLocation(rad_lat, rad_lon, deg_lat, deg_lon)
+        
+    @classmethod
+    def from_radians(cls, rad_lat, rad_lon):
+        deg_lat = math.degrees(rad_lat)
+        deg_lon = math.degrees(rad_lon)
+        return GeoLocation(rad_lat, rad_lon, deg_lat, deg_lon)
+    
+    
+    def __init__(
+            self,
+            rad_lat,
+            rad_lon,
+            deg_lat,
+            deg_lon
+    ):
+        self.rad_lat = float(rad_lat)
+        self.rad_lon = float(rad_lon)
+        self.deg_lat = float(deg_lat)
+        self.deg_lon = float(deg_lon)
+        self._check_bounds()
+        
+    def __str__(self):
+        degree_sign= u'\N{DEGREE SIGN}'
+        return ("({0:.4f}deg, {1:.4f}deg) = ({2:.6f}rad, {3:.6f}rad)").format(
+            self.deg_lat, self.deg_lon, self.rad_lat, self.rad_lon)
+        
+    def _check_bounds(self):
+        if (self.rad_lat < GeoLocation.MIN_LAT 
+                or self.rad_lat > GeoLocation.MAX_LAT 
+                or self.rad_lon < GeoLocation.MIN_LON 
+                or self.rad_lon > GeoLocation.MAX_LON):
+            raise Exception("Illegal arguments")
+            
+    def distance_to(self, other, radius=EARTH_RADIUS):
+        '''
+        Computes the great circle distance between this GeoLocation instance
+        and the other.
+        '''
+        return radius * math.acos(
+                math.sin(self.rad_lat) * math.sin(other.rad_lat) +
+                math.cos(self.rad_lat) * 
+                math.cos(other.rad_lat) * 
+                math.cos(self.rad_lon - other.rad_lon)
+            )
+            
+    def bounding_locations(self, distance, radius=EARTH_RADIUS):
+        '''
+        Computes the bounding coordinates of all points on the surface
+        of a sphere that has a great circle distance to the point represented
+        by this GeoLocation instance that is less or equal to the distance argument.
+        
+        Param:
+            distance - the distance from the point represented by this GeoLocation
+                       instance. Must be measured in the same unit as the radius
+                       argument (which is kilometers by default)
+            
+            radius   - the radius of the sphere. defaults to Earth's radius.
+            
+        Returns a list of two GeoLoations - the SW corner and the NE corner - that
+        represents the bounding box.
+        '''
+        
+        if radius < 0 or distance < 0:
+            raise Exception("Illegal arguments")
+            
+        # angular distance in radians on a great circle
+        rad_dist = distance / radius
+        
+        min_lat = self.rad_lat - rad_dist
+        max_lat = self.rad_lat + rad_dist
+        
+        if min_lat > GeoLocation.MIN_LAT and max_lat < GeoLocation.MAX_LAT:
+            delta_lon = math.asin(math.sin(rad_dist) / math.cos(self.rad_lat))
+            
+            min_lon = self.rad_lon - delta_lon
+            if min_lon < GeoLocation.MIN_LON:
+                min_lon += 2 * math.pi
+                
+            max_lon = self.rad_lon + delta_lon
+            if max_lon > GeoLocation.MAX_LON:
+                max_lon -= 2 * math.pi
+        # a pole is within the distance
+        else:
+            min_lat = max(min_lat, GeoLocation.MIN_LAT)
+            max_lat = min(max_lat, GeoLocation.MAX_LAT)
+            min_lon = GeoLocation.MIN_LON
+            max_lon = GeoLocation.MAX_LON
+        
+        return [ GeoLocation.from_radians(min_lat, min_lon) , 
+            GeoLocation.from_radians(max_lat, max_lon) ]
 
 def main(argv=None): # IGNORE:C0111
     '''Command line options.'''
@@ -80,6 +199,7 @@ USAGE
         parser.add_argument("-c", "--combine", dest="combineAllTracksForDate", action="store_true", help="combine all tracks that match the datetime specified into one output file", default=False)
         parser.add_argument("-m", "--merge", dest="mergeFiles", action="store_true", help="merge tracks from one or more input file into a single output file", default=False)
         parser.add_argument("-p", "--pretty", dest="prettify", action="store_true", help="prettify the output file", default=False)
+        parser.add_argument("-t", "--thinDistance", dest="thinDistance", help="thin out track points to reduce file size by removing all points within this distance in kilometers from each other [default: %(default)s]", default=0)
         parser.add_argument("-d", "--date", dest="trackDatetimes", action="append", help="extract track from datetime (ISO format yyyy-mm-dd [hh:mm:ss]). Can be specified multiple times to append", default=[])
         parser.add_argument("-o", "--output", dest="outputFile", help="direct output to this destination [default: %(default)s]", default='extracted_track.gpx')
         parser.add_argument("-i", "--input", dest="inputFiles", action="append", help="source gpx file containing one or more tracks. Can be specified multiple times to search or combine multiple files", default=[])
@@ -95,6 +215,7 @@ USAGE
         outputFile = args.outputFile
         combineAllTracksForDate = args.combineAllTracksForDate
         mergeFiles = args.mergeFiles
+        thinDistance = float(args.thinDistance)
         prettify = args.prettify
         if len(inputFiles) == 0:
             sys.stderr.write(program_name + ":\n")
@@ -183,6 +304,25 @@ USAGE
                                     combinedTracks = combinedTracks + '<trk><name>Active Log: Combined track from ' + trackDatetimeObject.strftime('%Y-%m-%d') + '</name>'
                                 for trackName in tracksForDates[trackDatetimeString]['trackNames']:
                                     track = trackName.find_parent('trk')
+                                    if thinDistance > 0:
+                                        trackSegs = track.find_all('trkseg')
+                                        for trackSeg in trackSegs:
+                                            trackPoint = trackSeg.find('trkpt')
+                                            if trackPoint is not None and trackPoint['lat'] is not None and trackPoint['lon'] is not None:
+                                                geoLocation = GeoLocation.from_degrees(float(trackPoint['lat']), float(trackPoint['lon']))
+                                                nextTrackPoint = trackPoint.find_next_sibling('trkpt')
+                                                if nextTrackPoint is not None and nextTrackPoint['lat'] is not None and nextTrackPoint['lon'] is not None:
+                                                    nextGeoLocation = GeoLocation.from_degrees(float(nextTrackPoint['lat']), float(nextTrackPoint['lon']))
+                                                    foundLastTrackPoint = False
+                                                    while not foundLastTrackPoint:  
+                                                        nextGeoLocation = GeoLocation.from_degrees(float(nextTrackPoint['lat']), float(nextTrackPoint['lon']))
+                                                        tempTrackPoint = nextTrackPoint.find_next_sibling('trkpt')
+                                                        foundLastTrackPoint = tempTrackPoint is None
+                                                        if geoLocation.distance_to(nextGeoLocation) > thinDistance or foundLastTrackPoint:
+                                                            geoLocation = nextGeoLocation
+                                                        else:
+                                                            nextTrackPoint.decompose()
+                                                        nextTrackPoint = tempTrackPoint
                                     if combineAllTracksForDate:
                                         # remove the old name tag if combining
                                         track.find('name').decompose()
